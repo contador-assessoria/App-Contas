ad529752aa5a396685a27bd4bca97ecfcb6840db
contador-assessoria <140221229+contador-assessoria@users.noreply.github.com>
2025-09-07 15:51:26 -0300

    Update __init__.py

---
+++ src/ui/components/__init__.py
@@ -0,0 +1,49 @@
+"""
+User interface module for expense tracking application.
+Contains all GUI components and dialogs.
+"""
+
+from .main_window import MainWindow
+from .widgets import (
+    MonthWidget,
+    DashboardWidget,
+    HistoryWidget,
+    ChartsWidget
+)
+from .dialogs import (
+    SettingsDialog,
+    ExportDialog,
+    AboutDialog,
+    ExpenseDialog
+)
+from .components import (
+    CustomSpinBox,
+    CurrencyInput,
+    DatePicker,
+    IconButton,
+    StatusIndicator
+)
+
+__all__ = [
+    # Main window
+    'MainWindow',
+    
+    # Widgets
+    'MonthWidget',
+    'DashboardWidget', 
+    'HistoryWidget',
+    'ChartsWidget',
+    
+    # Dialogs
+    'SettingsDialog',
+    'ExportDialog',
+    'AboutDialog',
+    'ExpenseDialog',
+    
+    # Components
+    'CustomSpinBox',
+    'CurrencyInput',
+    'DatePicker',
+    'IconButton',
+    'StatusIndicator'
+]
