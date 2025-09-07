8cea84dec90f6008b80308b1914f024784820b5b
contador-assessoria <140221229+contador-assessoria@users.noreply.github.com>
2025-09-07 16:32:20 -0300

    Update __init__.py

---
+++ src/ui/dialogs/__init__.py
@@ -0,0 +1,11 @@
+"""Dialog classes used across the application."""
+
+from .about_dialog import AboutDialog
+from .export_dialog import ExportDialog
+from .settings_dialog import SettingsDialog
+
+__all__ = [
+    "AboutDialog",
+    "ExportDialog",
+    "SettingsDialog",
+]
